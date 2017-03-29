d4ab89c94e2d09072aae96f4f3f13661c047dee1
Pham Trong Tri <tri.phamtrong@toshiba-tsdv.com>
2017-03-29 17:47:31 +0700

    First commit

---
+++ MySpider/spiders/stack_spider.py
@@ -0,0 +1,22 @@
+from scrapy import Spider
+from scrapy.selector import Selector
+from MySpider.items import MySpiderItem
+
+
+class StackSpider(Spider):
+    name = "stack"
+    allowed_domains = ["stackoverflow.com"]
+    start_urls = [
+        "http://stackoverflow.com/questions?pagesize=50&sort=newest",
+    ]
+
+    def parse(self, response):
+        questions = Selector(response).xpath('//div[@class="summary"]/h3')
+
+        for question in questions:
+            item = MySpiderItem()
+            item['title'] = question.xpath(
+                'a[@class="question-hyperlink"]/text()').extract()[0]
+            item['url'] = question.xpath(
+                'a[@class="question-hyperlink"]/@href').extract()[0]
+            yield item
